b83bc0e64357f8eec26fb909ce987eac19cc2e56
Andrew-Bourgeois <experiment626@macbook-pro.lan>
2023-08-22 23:28:49 -0400

    updated kdatetime, timedelta and most active code sections in both appy.py and climate.ipynb

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy import func
 from flask import Flask, jsonify
 
+from datetime import datetime as dt, timedelta as td
+
 #################################################
 # Database Setup
 #################################################
@@ -52,10 +54,18 @@ def precipitation():
 
     """Return results of precipitation analysis (retrieve only the last 12 months of data) to a dictionary using date as the 'key' and 'prcp' as the value"""
 
-    # Query the last years precipitation
+    # find most recent observation
+    most_recent = session.query(Measurement.date).\
+        order_by(Measurement.date.desc()).first()
+    
+    # convert most recent to datetime obj and find date of one year prior
+    most_recent_obs = dt.strptime(most_recent[0], '%Y-%m-%d')
+    year_ago_date = most_recent_obs - td(days=365)
+
+    # Query the last year's observation dates and precipitation
     last_12_months = session.query(Measurement.date, Measurement.prcp).\
         order_by(Measurement.date).\
-        filter(Measurement.date >= '2016-08-23').all()
+            filter(Measurement.date >= year_ago_date).all()
 
     session.close()
 
@@ -93,29 +103,56 @@ def stations():
     return jsonify(all_stations)
 
 
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     # Create a session from Python to the DB
-#     session = Session(engine)
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create a session from Python to the DB
+    session = Session(engine)
 
-#     # Query the dates and temperature observations of the most-active station for the previous year of data.
+    # Query the dates and temperature observations of the most-active station for the previous year of data.
+    # Find the most active station
+    most_active_station = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
 
-#     # Return a JSON list of temperature observations for the previous year.
+    most_active_no = most_active_station[0]
+
+    # find last observation date
+    last_obs_date = session.query(Measurement.date).\
+        order_by(Measurement.date.desc()).\
+            filter(Measurement.station == most_active_no).first()
+
+    # convert last obs date to datetime obj and calculate date a year ago
+    # convert most recent to datetime obj and find date of one year prior
+    last_obs = dt.strptime(last_obs_date[0], '%Y-%m-%d')
+    year_ago_date = last_obs - td(days=365)
+
+    # query dates and temperatures
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= year_ago_date).\
+            filter(Measurement.station == most_active_no).all()
+
+    most_active_tobs = [{"station": most_active_no}]
+    for date, tobs in results:
+        station_dict = {}
+        station_dict["date"] = date
+        station_dict["temperature"] = tobs
+        most_active_tobs.append(station_dict)
+
+    # Return a JSON list of temperature observations for the previous year.
+    return jsonify(most_active_tobs)
 
 
 # # ---DYNAMIC ROUTES---
-# @app.route("/api/v1.0/<start>")
-# def tobs():
-#     # Create a session from Python to the DB
-#     session = Session(engine)
+@app.route("/api/v1.0/<start>")
+def start_var():
+    session = Session(engine)
 
-#     """
-#         Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+    """
+        Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
 
-#         For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+        For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
 
-#         For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
-#     """
+        For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+    """
+    
 
 
 # @app.route("/api/v1.0/<start>/<end>")
