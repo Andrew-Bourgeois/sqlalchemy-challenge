a94c0c4a2692fd0221e4155018b8c7f770194165
Andrew-Bourgeois <experiment626@MacBook-Pro.local>
2023-08-22 21:42:00 -0400

    Added some code and Updated others

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -18,8 +18,8 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
-station = Base.classes.station
-measurement = Base.classes.measurement
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -53,18 +53,21 @@ def precipitation():
     """Return results of precipitation analysis (retrieve only the last 12 months of data) to a dictionary using date as the 'key' and 'prcp' as the value"""
 
     # Query the last years precipitation
-    last_12_months = session.query("??????")
+    last_12_months = session.query(Measurement.date, Measurement.prcp).\
+        order_by(Measurement.date).\
+        filter(Measurement.date >= '2016-08-23').all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of date_precip
     date_precip = []
-    for date, prcp in results:
+    for date, prcp in last_12_months:
         date_prcp_dict = {}
         date_prcp_dict["date"] = date
         date_prcp_dict["prcp"] = prcp
+        date_precip.append(date_prcp_dict)
 
-    return jsonify(last_12_months)
+    return jsonify(date_precip)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -72,45 +75,61 @@ def stations():
     session = Session(engine)
 
     """Return jsonified data of all stations in the database"""
-    results = session.query()
+
+    results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
 
     session.close()
 
+    all_stations = []
+    for station, name, lat, lon, elev in results:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = lat
+        station_dict["longitude"] = lon
+        station_dict["elevation"] = elev
+        all_stations.append(station_dict)
 
-@app.route("/api/v1.0/tobs")
-def tobs():
-    # Create a session from Python to the DB
-    session = Session(engine)
-
-    # Query the dates and temperature observations of the most-active station for the previous year of data.
-
-    # Return a JSON list of temperature observations for the previous year.
-
-
-# ---DYNAMIC ROUTES---
-@app.route("/api/v1.0/<start>")
-def tobs():
-    # Create a session from Python to the DB
-    session = Session(engine)
-
-    """
-        Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
-
-        For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
-
-        For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
-    """
+    return jsonify(all_stations)
 
 
-@app.route("/api/v1.0/<start>/<end>")
-def tobs():
-    # Create a session from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+#     # Create a session from Python to the DB
+#     session = Session(engine)
 
-    """
-        Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+#     # Query the dates and temperature observations of the most-active station for the previous year of data.
 
-        For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+#     # Return a JSON list of temperature observations for the previous year.
 
-        For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
-    """
+
+# # ---DYNAMIC ROUTES---
+# @app.route("/api/v1.0/<start>")
+# def tobs():
+#     # Create a session from Python to the DB
+#     session = Session(engine)
+
+#     """
+#         Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+
+#         For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+
+#         For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+#     """
+
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def tobs():
+#     # Create a session from Python to the DB
+#     session = Session(engine)
+
+#     """
+#         Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+
+#         For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+
+#         For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+#     """
+
+if __name__ == '__main__':
+    app.run(debug=True)
